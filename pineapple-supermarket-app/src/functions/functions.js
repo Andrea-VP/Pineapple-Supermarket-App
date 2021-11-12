28be13b851ada890130dd754124e24c68055f5b5
Carla Sanchez Correa <carlasanchezcorrea@gmail.com>
2021-11-12 17:41:56 -0500

    add folfer react

---
+++ pineapple-supermarket-app/src/functions/functions.js
@@ -0,0 +1,31 @@
+// Vista log In
+function send(){
+    //console.log('Presionando version 2')
+    let input = document.querySelector("#name")
+    console.log(input.value)
+}
+
+function create (e){ 
+    e.preventDefault()
+    console.log('Presionando version 2')
+    let input = document.querySelectorAll(".register")
+    input.forEach((i)=>{
+        console.log(i.value)
+    })
+}
+
+function edit (){
+    //console.log('Presionando version 2')
+    let input = document.querySelectorAll(".register")
+    console.log(input.value)
+}
+
+function erase (){
+    //console.log('Presionando version 2')
+    let input = document.querySelectorAll(".register")
+    console.log(input.value)
+}
+
+module.exports = {
+    send,create,edit,erase
+}
