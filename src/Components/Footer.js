ecb73b5b54e70cac088eb11911d1bdba97c51d14
Melanie Rodas <mrodasl@uni.pe>
2021-11-22 15:39:59 -0500

    added claseName footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,17 +5,17 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer>
-        <div class="container_footer">
-            <div class="footer__logo"></div>
-            <div class="footer__link1"><a></a></div>
-            <div class="footer__link2"><a></a></div>
-            <div class="footer__link3"><a></a></div>
-            <div class="footer__social">
+        <div className="container_footer">
+            <div className="footer__logo"></div>
+            <div className="footer__link1"><a></a></div>
+            <div className="footer__link2"><a></a></div>
+            <div className="footer__link3"><a></a></div>
+            <div className="footer__social">
                 <a href=""><i class="fab fa-github"></i></a>
                 <a href=""><i class="fab fa-linkedin"></i></a>
                 <a></a>
             </div>
-            <div class="footer__copyright">
+            <div className="footer__copyright">
                 <p> © 2021 Copyright, All rights reserved</p>
             </div>
           </div>     
